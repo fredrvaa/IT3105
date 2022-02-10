85ccf48890ea9c2875018908567d579ec6373a01
Fredrik <fredrvaa@stud.ntnu.no>
2022-02-10 15:48:06 +0100

    WIP: Config parser

---
+++ project1/main.py
@@ -0,0 +1,20 @@
+import argparse
+
+from environments.environment import Environment
+from learner.actor_critic import ActorCritic
+from utils.config_parser import ConfigParser
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--config', help='path/to/config/file', required=True)
+args = parser.parse_args()
+
+config_parser = ConfigParser(args.config)
+
+environment: Environment = config_parser.environment
+
+actor_critic: ActorCritic = config_parser.actor_critic
+
+fit_kwargs = config_parser.fit_parameters
+
+actor_critic.fit(**fit_kwargs)
+actor_critic.visualize()
---
+++ project1/utils/config_parser.py
@@ -0,0 +1,62 @@
+import inspect
+
+import yaml
+
+from environments.environment import Environment
+from environments.cartpole import CartPole
+from environments.towers_of_hanoi import TowersOfHanoi
+from environments.gambler import Gambler
+from learner.actors.actor import Actor
+from learner.critics.critic import Critic
+from learner.critics.table_critic import TableCritic
+from learner.actor_critic import ActorCritic
+
+STRING_EXCEPTIONS = ['name', 'checkpoint_folder']
+
+
+class ConfigParser:
+    def __init__(self, config_file: str):
+        with open(config_file, "r") as stream:
+            self._config = yaml.safe_load(stream)
+
+        self.environment: Environment = self._get_environment()
+        self.actor_critic: ActorCritic = self._get_actor_critic()
+        self.fit_parameters: dict = self._get_fit_parameters()
+
+    def _parse_config(self, config) -> dict:
+        parsed_config = {}
+        for k, v in config.items():
+            if v is not None:
+                if type(v) is dict:
+                    parsed_config[k] = self._parse_config(v)
+                elif type(v) is list:
+                    parsed_config[k] = [self._parse_config(x) for x in v]
+                elif type(v) is str and k not in STRING_EXCEPTIONS:
+                    parsed_config[k] = eval(v)
+                else:
+                    parsed_config[k] = v
+                parsed_config
+        return parsed_config
+
+    def _get_environment(self) -> Environment:
+        environment = self._parse_config(self._config['environment_type'])
+        kwargs = self._parse_config(self._config['environment_params'])
+        return environment(**kwargs)
+
+    def _get_actor(self) -> Actor:
+        actor = self._parse_config(self._config['actor_type'])
+        kwargs = self._parse_config(self._config['actor_params'])
+        return actor(environment=self.environment, **kwargs)
+
+    def _get_critic(self) -> Critic:
+        critic = self._parse_config(self._config['critic_type'])
+        kwargs = self._parse_config(self._config['critic_params'])
+        return critic(environment=self.environment, **kwargs)
+
+    def _get_actor_critic(self) -> ActorCritic:
+        actor = self._get_actor()
+        critic = self._get_critic()
+        return ActorCritic(environment=self._environment, actor=actor, critic=critic)
+
+    def _get_fit_parameters(self) -> dict:
+        return self._parse_config(self._config['fit'])
