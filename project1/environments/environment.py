8e0f876805c3d11f374665d3c39b6f98016af3a6
Fredrik <fredrvaa@stud.ntnu.no>
2022-02-10 15:47:47 +0100

    Add type hints to environment

--- project1/environments/environment.py
+++ project1/environments/environment.py
@@ -1,23 +1,25 @@
 from abc import ABC, abstractmethod
 
+from environments.actions import Actions
+
 
 class Environment(ABC):
     @abstractmethod
-    def initialize(self):
+    def initialize(self) -> tuple:
         raise NotImplementedError('Subclasses must implement initialize()')
 
     @abstractmethod
-    def next(self):
+    def next(self) -> tuple:
         raise NotImplementedError('Subclasses must implement next()')
 
     @property
     @abstractmethod
-    def state_shape(self):
+    def state_shape(self) -> tuple:
         return
 
     @property
     @abstractmethod
-    def actions(self):
+    def actions(self) -> Actions:
         return
 
 
