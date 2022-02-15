667cd25ca4495b5ea0a2798f3fa730acefb7d967
Fredrik <fredrvaa@stud.ntnu.no>
2022-02-15 14:37:31 +0100

    Document action class with docstrings

--- project1/environments/actions.py
+++ project1/environments/actions.py
@@ -2,8 +2,18 @@ import numpy as np
 
 
 class Actions:
+    """Class used as a common interface for actions."""
+
     def __init__(self, n: int):
+        """
+        :param n: Number of possible actions
+        """
         self.n = n
 
-    def random(self):
+    def random(self) -> int:
+        """Returns a random action.
+
+        :return: Integer corresponding to an action.
+        """
+
         return np.random.randint(self.n)
